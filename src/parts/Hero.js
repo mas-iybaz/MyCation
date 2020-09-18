4609682fb33ee3f023af26cc7e9caa98a31eb780
mas-iybaz <muhammadiqbal.workspace@gmail.com>
2020-09-19 06:56:39 +0700

    create hero parts

---
+++ src/parts/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import imageHero from "assets/images/img-hero.jog";
+import imageHeroFrame from "assets/images/img-hero-frame.jog";
+import iconCities from "assets/images/icons/icon-cities.svg";
+import iconTraveler from "assets/images/icons/icon-traveler.svg";
+import iconTreasure from "assets/images/icons/icon-treasure.svg";
+
+export default function Hero(props) {
+  return <div></div>;
+}
