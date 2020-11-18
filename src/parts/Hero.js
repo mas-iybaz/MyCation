2ab94b6b8c528460de6e64185b703518f5ca3d68
mas-iybaz <muhammadiqbal.workspace@gmail.com>
2020-11-18 12:23:21 +0700

    styling hero

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -9,6 +9,8 @@ import iconTreasure from "assets/images/icons/icon-treasure.svg";
 
 import Button from "elements/Button";
 
+import formatNumbering from "utils/formatNumbering";
+
 export default function Hero(props) {
   function showMostPicked() {
     window.scrollTo({
@@ -21,11 +23,14 @@ export default function Hero(props) {
     <section className="container pt-4">
       <div className="row align-items-center">
         <div className="col-auto pr-5" style={{ width: 530 }}>
-          <h1 className="h2 font-weight-bold line-height-1 mb-3">
+          <h1 className="font-weight-bold line-height-1 mb-3">
             Forget Busy Work, <br />
             Start New Vacation
           </h1>
-          <p className="mb-5 font-weight-light text-gray-500 w-75">
+          <p
+            className="mb-4 font-weight-light text-gray-500 w-75"
+            style={{ lineHeight: "170%" }}
+          >
             We provide what you need to enjoy your holiday with family. Time to
             make another memorable moments
           </p>
@@ -46,7 +51,7 @@ export default function Hero(props) {
                 alt={`${props.data.travelers} travelers`}
               />
               <h6 className="mt-3">
-                {props.data.travelers}{" "}
+                {formatNumbering(props.data.travelers)}{" "}
                 <span className="text-gray-500 font-weight-light">
                   travelers
                 </span>
@@ -59,7 +64,7 @@ export default function Hero(props) {
                 alt={`${props.data.treasures} treasures`}
               />
               <h6 className="mt-3">
-                {props.data.treasures}{" "}
+                {formatNumbering(props.data.treasures)}{" "}
                 <span className="text-gray-500 font-weight-light">
                   treasures
                 </span>
@@ -72,7 +77,7 @@ export default function Hero(props) {
                 alt={`${props.data.cities} cities`}
               />
               <h6 className="mt-3">
-                {props.data.cities}{" "}
+                {formatNumbering(props.data.cities)}{" "}
                 <span className="text-gray-500 font-weight-light">cities</span>
               </h6>
             </div>
